a09589e5fad40fbdd714cf7cd0ac00f9445f5b64
ortza000 <ortza18@gmail.com>
2020-01-10 21:08:54 +0700

    5

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/index2', 'HomeController@index')->name('index2');
