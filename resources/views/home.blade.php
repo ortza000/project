37f584b2b8dbe34f41997ae54f1881d716efe8a7
ortza000 <ortza18@gmail.com>
2020-01-10 21:10:34 +0700

    ooooo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -89,7 +89,6 @@
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
